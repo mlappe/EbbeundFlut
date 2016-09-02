bcaa8cf68ec4667fafa597bfe1c685791e8b6eff
MaxAdmin <lappe@cl.uni-heidelberg.de>
2016-09-02 19:05:19 +0200

    changed the structure of the directory

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 import Gamestate as Gamestate
-import interface as interface
-import sampleai as  randomai
+import Interface.interface
+import Ai.sampleai
 
 
-ai1 = randomai.sampleai()
-ai2 = randomai.sampleai()
+ai1 = Ai.sampleai.sampleai()
+ai2 = Ai.sampleai.sampleai()
 
 
 
-t = interface.terminal()
+t = Interface.interface.terminal()
 g = Gamestate.Gamestate(ai1=ai1,ai2=ai2,interface=t)
 
 
@@ -18,7 +18,6 @@ while True:
 	g._new_turn()
 	player = g._get_ac_player()
 	player.ai.set_card(g)
-	t._display_field(g)
 	player.ai.move(g)
 	#input()
 
